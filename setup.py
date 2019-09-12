4c7114946575ba81b97f6e05bb78d624027f6b92
jenkins <jenkins@eva.bot>
2019-09-12 18:13:24 +0000

    Moving development to version 1.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.environ.get('include_tests'):
 
 setup(
     name='noronha-dataops',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/athosgvag/noronha-dataops',
     author='Gustavo Vargas Castilhos',
     author_email='gvargasc@everis.com',
