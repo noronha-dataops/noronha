8430da24ebad45902d152e3ca6e43ea9a76d703d
gvargasc <gustavo.vargas.castilhos@everis.com>
2019-09-19 12:26:36 -0300

    lazy import of papermill to avoid bug in host using methods from tooling

--- noronha/tools/__init__.py
+++ noronha/tools/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from noronha.common.utils import am_i_on_board
+
+
 __all__ = ['notebook', 'online']
 
-from noronha.tools.notebook import NoronhaEngine
+if am_i_on_board():
+    from noronha.tools.notebook import NoronhaEngine
