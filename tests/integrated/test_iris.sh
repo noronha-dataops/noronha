a7daba708a2fdf5deaf67d7454fad1e42bdfeec1
gvargasc <gustavo.vargas.castilhos@everis.com>
2019-09-30 14:55:36 -0300

    better logs parsing in test

--- tests/integrated/test_iris.sh
+++ tests/integrated/test_iris.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-set -x
-
 examples=`python -c 'from noronha.common.constants import Package; print(Package.EXAMPLES)'` &>/dev/null
 
 cp -r ${examples}/iris/* .
 
-log=`sh script.sh 2>&1`
+log=`set -x && sh script.sh 2>&1`
 
-set +x
+echo -e "TEST LOGS:\n${log}"
 
 result=`echo -e "${log}" | tail -n 1`
 
-label=`python -c "import json; print(json.loads('${result}').get('result'))" 2>/dev/null`
+label=`python -c "import json; print(json.loads('${result}').get('result'))"`
 
 if [[ "${label}" == "setosa" ]] ; then
     exit 0
