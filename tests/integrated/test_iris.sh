5cbdf4105cdec7d687d402b0136b3347165a15ae
gvargasc <gustavo.vargas.castilhos@everis.com>
2019-09-30 10:59:36 -0300

    better test result verification

--- tests/integrated/test_iris.sh
+++ tests/integrated/test_iris.sh
@@ -6,6 +6,16 @@ examples=`python -c 'from noronha.common.constants import Package; print(Package
 
 cp -r ${examples}/iris/* .
 
-sh script.sh
+log=`sh script.sh`
 
-exit $?
+echo -e "${log}"
+
+result=`echo -e "${log}" | tail -n 1`
+
+expected='{"result": "setosa", "metadata": {"datetime": "2019-09-30 13:51:55", "model_version": "experiment-v1"}}'
+
+if [[ "${result}" == "${expected}" ]] ; then
+    exit 0
+else
+    exit 1
+fi
