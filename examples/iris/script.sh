ff21e25b30c2d8cc827b3b072e4e33f98745b5d2
gvargasc <gustavo.vargas.castilhos@everis.com>
2019-09-30 19:28:14 -0300

    avoiding echo command in middle of curl output

--- examples/iris/script.sh
+++ examples/iris/script.sh
@@ -66,5 +66,4 @@ http://127.0.0.1:30050/predict \
 curl -X POST \
 -H 'Content-Type: application/JSON' \
 --data '{"project": "botanics", "deploy": "homolog", "data": [1,2,3,4]}' \
-http://127.0.0.1:30080 \
-&& echo
+http://127.0.0.1:30080
