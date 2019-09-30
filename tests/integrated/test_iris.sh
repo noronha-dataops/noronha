1e68dc705163070ce3905c08b13ac2de55826908
gvargasc <gustavo.vargas.castilhos@everis.com>
2019-09-30 19:13:39 -0300

    better test output printing

--- tests/integrated/test_iris.sh
+++ tests/integrated/test_iris.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+delay=20
+
 examples=`python -c 'from noronha.common.constants import Package; print(Package.EXAMPLES)'` &>/dev/null
 
 cp -r ${examples}/iris/* .
@@ -10,7 +12,14 @@ echo -e "TEST LOGS:\n${log}"
 
 result=`echo -e "${log}" | tail -n 1`
 
+echo "Waiting ${delay} seconds"
+sleep ${delay}
+
+echo -e "TEST RESULT: ${result}"
+
 label=`python -c "import json; print(json.loads('${result}').get('result'))"`
+
+echo -e "LABEL: ${result}"
 
 if [[ "${label}" == "setosa" ]] ; then
     exit 0
