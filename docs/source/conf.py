8a42e468d5aa3c4028a15d46a10ae6920e3dce7a
jenkins <jenkins@eva.bot>
2020-01-06 18:15:27 +0000

    Moving development to version 1.3.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'everis'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.9'
+release = '1.3.10'
 
 
 # -- General configuration ---------------------------------------------------
