ea107647e63a91814f22d3d3d1027720f3189283
jenkins <jenkins@eva.bot>
2020-02-14 14:51:03 +0000

    Release v1.3.15

--- noronha/common/constants.py
+++ noronha/common/constants.py
@@ -11,7 +11,7 @@ class FrameworkConst(object):
     
     FW_NAME = 'noronha-dataops'
     FW_VERSION = '1.3.15'  # framework version
-    FW_TAG = 'develop'  # framework tag
+    FW_TAG = 'latest'  # framework tag
 
 
 class Perspective(object):
