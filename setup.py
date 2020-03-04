a188c6eb7dfe310061abe4b18221d0f18bb08e34
jenkins <jenkins@eva.bot>
2020-03-04 01:52:21 +0000

    Moving development to version 1.4.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.environ.get('include_tests'):
 
 setup(
     name='noronha-dataops',
-    version='1.4.0',
+    version='1.4.1',
     url='https://github.com/gvargasc/noronha-dataops',
     author='Gustavo Vargas Castilhos',
     author_email='gvargasc@everis.com',
