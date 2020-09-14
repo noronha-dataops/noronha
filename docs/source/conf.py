858e470c7f5de52589c9cea9dd67056cb655a72a
jenkins <jenkins@eva.bot>
2020-09-14 11:03:19 +0000

    Moving development to version 1.5.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'everis'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.3'
+release = '1.5.4'
 
 
 # -- General configuration ---------------------------------------------------
