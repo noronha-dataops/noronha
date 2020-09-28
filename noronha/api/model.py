3eaa7ce70c0b64a9b6b284c5ec77d04153fbb534
gcrocker <guilherme.crocker@gmail.com>
2020-09-28 20:34:21 -0300

    Revert "Model update and removal are now applied to Lightweight storage"
    This reverts commit 773b252183ef3b53793b5137d5334ac8334d055e.

--- noronha/api/model.py
+++ noronha/api/model.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from cassandra import InvalidRequest
-
 from noronha.api.ds import DatasetAPI
 from noronha.api.main import NoronhaAPI
 from noronha.api.movers import ModelVersionAPI
-from noronha.bay.utils import FileSpec, StoreHierarchy
-from noronha.bay.warehouse import get_lw_warehouse
 from noronha.common.annotations import validate
-from noronha.common.constants import WarehouseConst
 from noronha.common.logging import LOG
 from noronha.db.model import Model
 
@@ -34,14 +29,6 @@ class ModelAPI(NoronhaAPI):
                 report[key].append(obj.name)
         
         report.update(super().rm(name=name))
-
-        try:
-            get_lw_warehouse(section=WarehouseConst.Section.MODELS).drop_table(hierarchy=StoreHierarchy(name, 'dummy'))
-        except (AssertionError, InvalidRequest):
-            return report
-        except Exception as e:
-            LOG.debug("Did not update Cassandra. Error: {}".format(repr(e)))
-
         return report
     
     def lyst(self, _filter: dict = None, **kwargs):
@@ -69,24 +56,8 @@ class ModelAPI(NoronhaAPI):
         if kwargs.get('data_files'):
             LOG.warn("If 'data_files' definition is changed, old datasets may become unusable")
             self._decide("Do you want to proceed?", default=True, interrupt=True)
-
-        current: Model = self.doc.find_one(name=name)
         
-        updated: Model = super().update(
+        return super().update(
             filter_kwargs=dict(name=name),
             update_kwargs=kwargs
         )
-
-        if kwargs.get('model_files'):
-
-            try:
-                get_lw_warehouse(section=WarehouseConst.Section.MODELS).update_schema(
-                    hierarchy=StoreHierarchy(updated.name, 'dummy'),
-                    new_schema=[FileSpec.from_doc(d) for d in updated.model_files],
-                    old_schema=[FileSpec.from_doc(d) for d in current.model_files])
-            except (AssertionError, InvalidRequest):
-                return updated
-            except Exception as e:
-                LOG.debug("Did not update Cassandra. Error: {}".format(repr(e)))
-
-        return updated
